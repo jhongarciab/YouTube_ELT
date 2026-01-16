7092d6a3a077c8dd3a32f5cc71f496fd0cc875d2
Jhon S. García Barrera <jhons.garciab@uqvirtual.edu.co>
2026-01-15 21:25:17 -0500

    DAG 2: Update DB

--- dags/main.py
+++ dags/main.py
@@ -1,13 +1,14 @@
 from airflow import DAG
 import pendulum
 from datetime import datetime, timedelta
-
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from api.video_stats import (
     get_playlist_id,
     get_video_ids,
     extract_video_data,
     save_to_json,
 )
+from warehouse.dwh import staging_table, core_table
 
 # Define the local timezone
 local_tz = pendulum.timezone("America/Bogota")
@@ -23,19 +24,44 @@ default_args = {
     "start_date": datetime(2025, 1, 1, tzinfo=local_tz),
 }
 
+# Variables
+staging_schema = "staging"
+core_schema = "core"
+
+# DAG 1: produce_json
 with DAG(
     dag_id="produce_json",
     default_args=default_args,
-    description="A DAG to produce JSON file with raw data",
-    schedule="0 14 * * *",  # Everyday at 2 PM Bogotá time
+    description="DAG to produce JSON file with raw data",
+    schedule="0 14 * * *",
     catchup=False,
-) as dag:
-
+) as dag_produce:
     # Define tasks
     playlist_id = get_playlist_id()
     video_ids = get_video_ids(playlist_id)
     extract_data = extract_video_data(video_ids)
     save_to_json_task = save_to_json(extract_data)
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+    )
+    # Define dependencies
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_db
 
-    # Dependencies
-    playlist_id >> video_ids >> extract_data >> save_to_json_task
+# DAG 2: update_db
+with DAG(
+    dag_id="update_db",
+    default_args=default_args,
+    description="DAG to process JSON file and insert data into both staging and core schemas",
+    catchup=False,
+    schedule=None,
+) as dag_update:
+    # Define tasks
+    update_staging = staging_table()
+    update_core = core_table()
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality",
+    )
+    # Define dependencies
+    update_staging >> update_core >> trigger_data_quality
