29d9b791564e4f2d74b033f16756a0ba668cf4b6
Jhon S. García Barrera <jhons.garciab@uqvirtual.edu.co>
2026-01-16 10:15:43 -0500

    Pytest & DAGs re-structure

--- dags/main.py
+++ dags/main.py
@@ -42,8 +42,12 @@ with DAG(
     video_ids = get_video_ids(playlist_id)
     extract_data = extract_video_data(video_ids)
     save_to_json_task = save_to_json(extract_data)
+    trigger_update_db = TriggerDagRunOperator(
+        task_id="trigger_update_db",
+        trigger_dag_id="update_db",
+    )
     # Define dependencies
-    playlist_id >> video_ids >> extract_data >> save_to_json_task
+    playlist_id >> video_ids >> extract_data >> save_to_json_task >> trigger_update_db
 
 # DAG 2: update_db
 with DAG(
@@ -56,8 +60,12 @@ with DAG(
     # Define tasks
     update_staging = staging_table()
     update_core = core_table()
+    trigger_data_quality = TriggerDagRunOperator(
+        task_id="trigger_data_quality",
+        trigger_dag_id="data_quality",
+    )
     # Define dependencies
-    update_staging >> update_core
+    update_staging >> update_core >> trigger_data_quality
 
 # DAG 3: data_quality
 with DAG(
